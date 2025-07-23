dd2aadf5ce3747c224c4ad7800c2d646eed27e30
sahal <sahal@Sahalpk>
2025-07-23 12:24:22 +0530

    hello world

--- shoppingMall/src/entity/User.java
+++ shoppingMall/src/entity/User.java
@@ -3,7 +3,7 @@ package entity;
 public class User {
 
 	public static void main(String[] args) {
-		System.out.println("hi sahal");
+		System.out.println("hi mughilan");
 		// TODO Auto-generated method stub
 
 	}
